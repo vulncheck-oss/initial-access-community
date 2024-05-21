8947f2cc24d3301c6568c18c4e85e8babbd4aad1
Jacob Baines <113205286+j-baines@users.noreply.github.com>
2024-05-21 14:54:41 -0400

    Merge pull request #600 from vulncheck/tech-debt-testing
    #596 Added Tomcat scanners test. Added actions to build and test. Upd…
    
    GitOrigin-RevId: 3fb1f32d35b09d1b09aaf087dfc3ba290ba7dc6c

--- feed/make-all-builddir.sh
+++ feed/make-all-builddir.sh
@@ -10,7 +10,7 @@ function run_make_recursive() {
         if [ -d "$dir" ] && [ "$(basename "$dir")" != "build" ]; then
             if [ -e "$dir/Makefile" ]; then
                 echo "Entering directory: $dir"
-                (cd "$dir" && rm go.sum && go mod tidy && make compile) || status=1
+                (cd "$dir" && make compile) || status=1
             fi
             run_make_recursive "$dir" || status=1
         fi
