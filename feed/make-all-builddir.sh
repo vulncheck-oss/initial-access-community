45b0b7b49f80edae2dfa90346370fb050958cf56
Jacob Baines <113205286+j-baines@users.noreply.github.com>
2024-09-13 11:42:37 -0400

    Merge pull request #1118 from vulncheck/manifestdestiny
    Create manifest generation and adapted db-scanner to use it
    
    GitOrigin-RevId: 5d573bb60c8764d18a53fae2e98b2c2e279864a2

--- feed/make-all-builddir.sh
+++ feed/make-all-builddir.sh
@@ -11,7 +11,16 @@ function run_make_recursive() {
             if [ -e "$dir/Makefile" ]; then
                 if [ -f "$dir/go.mod" ]; then
                     echo "Entering directory: $dir"
-                    (cd "$dir" && make compile && cp ./build/cve-* /tmp/go-exploit-all) || status=1
+                    (cd "$dir" && make compile) || status=1
+                    for file in "$dir/build/cve-"*; do
+                        if [ -f "$file" ]; then
+                            # handle duplicate filenames by slapping a timestamp onto each binary
+                            filename=$(basename "$file")
+                            timestamp=$(date +%s%N)
+                            new_filename="${filename%.*}-$timestamp"
+                            cp "$file" "/tmp/go-exploit-all/$new_filename"
+                        fi
+                    done
                 fi
             fi
             run_make_recursive "$dir" || status=1
@@ -25,6 +34,5 @@ function run_make_recursive() {
     return $status
 }
 
-mkdir /tmp/go-exploit-all
+mkdir -p /tmp/go-exploit-all
 run_make_recursive .
-
