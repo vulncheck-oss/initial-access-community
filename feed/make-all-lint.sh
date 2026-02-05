6c96ff19a9d368c301f479c387c9d6b0d6365f5c
Jacob Baines <113205286+j-baines@users.noreply.github.com>
2026-02-05 10:26:21 -0500

    Merge pull request #3400 from vulncheck/1540-update
    Upgrade to go-exploit 1.54.0
    
    GitOrigin-RevId: 78d5dd8f7d74d14d24932b877946b1954f207287

--- feed/make-all-lint.sh
+++ feed/make-all-lint.sh
@@ -11,7 +11,7 @@ function run_make_recursive() {
             if [ -e "$dir/Makefile" ]; then
                 if [ -f "$dir/go.mod" ]; then
                     echo "Entering directory: $dir"
-                    (cd "$dir" && rm go.sum && go mod tidy && make all && make clean) || status=1
+                    (cd "$dir" && rm go.sum && go mod tidy && make compile && make clean) || status=1
                 fi
             fi
             run_make_recursive "$dir" || status=1
