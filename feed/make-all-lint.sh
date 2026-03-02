2ce5852454a02015b9c6cf722dda8fc65426fe83
Cale Black <terrorbyte@users.noreply.github.com>
2026-03-02 14:57:51 -0700

    Add a go mod tidy check to build all and PR gate (#3562)
    GitOrigin-RevId: 563dea13eddb71f13d8456649e5c987e25b1a503

--- feed/make-all-lint.sh
+++ feed/make-all-lint.sh
@@ -11,7 +11,7 @@ function run_make_recursive() {
             if [ -e "$dir/Makefile" ]; then
                 if [ -f "$dir/go.mod" ]; then
                     echo "Entering directory: $dir"
-                    (cd "$dir" && rm go.sum && go mod tidy && make compile && make clean) || status=1
+                    (cd "$dir" && (go mod tidy -diff || (printf "Go mod tidy failed on %s\\n" "$dir" 1>&2 && exit 1)) && make compile && make clean) || status=1
                 fi
             fi
             run_make_recursive "$dir" || status=1
