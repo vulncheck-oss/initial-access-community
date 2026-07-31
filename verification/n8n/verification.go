00d2d6b9d867fb2d841cf109aaf828696356c1cd
Jacob Baines <113205286+j-baines@users.noreply.github.com>
2026-07-31 10:05:02 -0400

    Merge pull request #4581 from vulncheck/cve-2026-27577
    CVE-2026-27577: n8n Authenticated Workflow Expression SpreadElement Sandbox Escape RCE
    GitOrigin-RevId: 8699e678398d76b0ab5e811293b218751a30edf9

--- verification/n8n/verification.go
+++ verification/n8n/verification.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/buger/jsonparser"
@@ -35,15 +36,62 @@ func DoCheckVersion(conf *config.Config, constraint string) exploit.VersionCheck
 	versionCli, err := jsonparser.GetString([]byte(body), "data", "versionCli")
 	if err != nil {
 		output.PrintfDebug("Could not find versionCli number: %s", err.Error())
+		output.PrintDebug("Proceeding with constants asset method")
+	} else {
+		exploit.StoreVersion(conf, versionCli)
+		if search.CheckSemVer(versionCli, constraint) {
+			return exploit.Vulnerable
+		}
+
+		return exploit.NotVulnerable
+	}
+
+	url := conf.GenerateURL("/")
+	resp, body, ok = protocol.HTTPGetCache(url)
+	if !ok {
+		return exploit.Unknown
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		output.PrintfDebug("Target responded with an unexpected status code: %d", resp.StatusCode)
 
 		return exploit.Unknown
 	}
-	exploit.StoreVersion(conf, versionCli)
-	if search.CheckSemVer(versionCli, constraint) {
-		return exploit.Vulnerable
+
+	val, found := search.XPath(body, `//link[contains(@href, "/assets/constants-")]/@href`)
+	if !found {
+		return exploit.Unknown
 	}
 
-	return exploit.NotVulnerable
+	constURL := conf.GenerateURL(val)
+	resp, body, ok = protocol.HTTPGetCache(constURL)
+	if !ok {
+		return exploit.Unknown
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		output.PrintfDebug("Constants asset page responded with unexpected status code: %d", resp.StatusCode)
+
+		return exploit.Unknown
+	}
+
+	// `n8n@2.10.0
+	rgx := regexp.MustCompile(`n8n@((?:\d+\.\d+)\.\d+)+`)
+	matches := rgx.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		output.PrintfDebug("Failed to find version on assets page, resp=%v, body=%s", resp, body)
+
+		return exploit.Unknown
+	}
+
+	version := matches[1]
+	exploit.StoreVersion(conf, version)
+
+	if !search.CheckSemVer(version, constraint) {
+		return exploit.NotVulnerable
+	}
+
+	return exploit.Vulnerable
 }
 
 func DoLogin(conf *config.Config, email, password string) (string, bool) {
